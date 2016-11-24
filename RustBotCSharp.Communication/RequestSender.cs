303f740aed6f49dd3a73c629468db194c97e6670
Carlos Miguel Correia da Costa <carloscosta.cmcc@gmail.com>
2016-11-24 19:59:28 +0000

    Added SEVDataSubscriber and RequestSender classes.

---
+++ RustBotCSharp.Communication/RequestSender.cs
@@ -0,0 +1,25 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+
+namespace RustBotCSharp.Communication
+{
+    public abstract class RequestSender
+    {
+        public RequestSocket RequestSocket { get; set; }
+        public ResponseSocket ResponseSocket { get; set; }
+
+        protected RequestSender(string requestSocketUrl = "@tcp://*:13370", string responseSocketUrl = ">tcp://localhost:13370")
+        {
+            RequestSocket = new RequestSocket(requestSocketUrl);
+            ResponseSocket = new ResponseSocket(responseSocketUrl);
+        }
+
+        public void SendRequest(string request)
+        {
+            RequestSocket.SendFrame(request);
+            ProcessResponse(ResponseSocket.ReceiveFrameString());
+        }
+
+        public abstract void ProcessResponse(string response);
+    }
+}
---
+++ RustBotCSharp.Communication/SEVDataSubscriber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace RustBotCSharp.Communication
+{
+    abstract class SEVDataSubscriber
+    {
+        public SubscriberSocket SubscriberSocket { get; set; }
+
+        protected SEVDataSubscriber(string subscriberUrl = "tcp://localhost:13370", string topic = "")
+        {
+            SubscriberSocket = new SubscriberSocket();
+            SubscriberSocket.Connect(subscriberUrl);
+            SubscriberSocket.Subscribe(topic);
+        }
+
+        public void StartReceivingData()
+        {
+            SubscriberSocket.ReceiveReady += SubscriberSocketOnReceiveReady;
+        }
+
+        private void SubscriberSocketOnReceiveReady(object sender, NetMQSocketEventArgs netMqSocketEventArgs)
+        {
+            ProcessData(ReceiveData());
+        }
+
+        public SEVData ReceiveData()
+        {
+            return SEVData.Parser.ParseFrom(SubscriberSocket.ReceiveFrameBytes());
+        }
+
+        public abstract void ProcessData(SEVData data);
+    }
+}
